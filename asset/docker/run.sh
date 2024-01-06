7bffaf9c53f1cee100d26f668721ebee753a57dd
chongh.ou <ochhgz@163.com>
2024-01-06 16:35:25 +0800

    修改broker启动参数

--- asset/docker/run.sh
+++ asset/docker/run.sh
@@ -5,7 +5,7 @@ nohup $ROCKETMQ_HOME/bin/mqnamesrv > /dev/null 2>&1 &
 echo "启动：mqnamesrv"
 
 # start mqbroker service
-nohup $ROCKETMQ_HOME/bin/mqbroker -n localhost:9876 > /dev/null 2>&1 &
+nohup $ROCKETMQ_HOME/bin/mqbroker -n $NAMESRV_ADDR > /dev/null 2>&1 &
 echo "启动：mqbroker"
 
 # start console service
